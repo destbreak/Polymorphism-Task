2d75a82b881fa2c634f7a2a5ee8339499f92e9f5
DestBreak <rey.hadiansyah7th@gmail.com>
2020-05-13 23:17:27 +0700

    Fix - Function

--- Program.cs
+++ Program.cs
@@ -16,8 +16,8 @@ namespace polymorphism
             Console.WriteLine("Pilih Printer : ");
             Console.WriteLine("1. Epson");
             Console.WriteLine("2. Canon");
-            Console.WriteLine("3. LaserJet");
-            Console.WriteLine("Nomor Printer [1..3] : ");
+            Console.WriteLine("3. LaserJet\n");
+            Console.Write("Nomor Printer [1..3] : ");
             int printerNumber = Convert.ToInt32(Console.ReadLine());
 
             if(printerNumber == 1) {
